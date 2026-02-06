5aafe78cd1b0517e9a5ee218492f3945c7c82864
Julian Sedano <ignovigenz@hotmail.com>
2026-02-06 00:56:46 +0000

    Skills section 3/4 done

--- My-Portfolio/src/components/Skills.tsx
+++ My-Portfolio/src/components/Skills.tsx
@@ -1,18 +1,117 @@
-function Skills(){
+function Skills() {
+
     return (
-         <section className="about pt-5">
+        <section className="about pt-5">
 
             <div className="container about-body mt-4">
 
                 <div className="mx-auto">
                     <h1 className="pt-5 text-center display-3 fw-normal" >Skills</h1>
 
-                    <div className="mt-5 p-5 row">
-                        <div className="web-design">
+                    <div className="mt-5 p-3 row">
+                        <div className="browser-card col-md-4">
+                            <div className="browser-header">
+                                <span className="dot red"></span>
+                                <span className="dot yellow"></span>
+                                <span className="dot green"></span>
 
+                            </div>
+
+                            <div className="browser-content text-center">
+                                <h3 className="fw-bold">PROGRAMMING LANGUAGES</h3>
+                                <div className="d-flex flex-column align-items-center">\
+                                    <div className="row">
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-javascript-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>JavaScript (ES6+)</span>
+                                        </div>
+
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-python-plain colored" style={{ fontSize: "50px", color: "#00b3ff" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>Python (3.14)</span>
+                                        </div>
+
+                                        <div className="col-md-6 d-flex flex-column align-items-center py-5 px-4">
+                                            <i className="devicon-php-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>PHP (8.1)</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        
-                        
+
+                        <div className="browser-card col-md-4">
+                            <div className="browser-header">
+                                <span className="dot red"></span>
+                                <span className="dot yellow"></span>
+                                <span className="dot green"></span>
+
+                            </div>
+
+                            <div className="browser-content text-center">
+                                <h3 className="fw-bold">FRONTEND TECHNOLOGIES</h3>
+                                <div className="d-flex flex-column align-items-center">
+                                    <div className="row py-4">
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-html5-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-css3-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4 py-5">
+                                            <i className="devicon-react-original colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4 py-5">
+                                            <i className="devicon-typescript-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>Python (3.14)</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="browser-card col-md-4">
+                            <div className="browser-header">
+                                <span className="dot red"></span>
+                                <span className="dot yellow"></span>
+                                <span className="dot green"></span>
+
+                            </div>
+
+                            <div className="browser-content text-center">
+                                <h3 className="fw-bold">BACKEND TECHNOLOGIES</h3>
+                                <div className="d-flex flex-column align-items-center">
+                                    <div className="row py-4">
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-html5-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4">
+                                            <i className="devicon-css3-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4 py-5">
+                                            <i className="devicon-react-original colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>React</span>
+                                        </div>
+                                        <div className="col-md-6 d-flex flex-column align-items-center px-4 py-5">
+                                            <i className="devicon-typescript-plain colored" style={{ fontSize: "50px" }}></i>
+                                            <span className="text-secondary" style={{ fontWeight: "700" }}>Python (3.14)</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
                     </div>
                 </div>
 
